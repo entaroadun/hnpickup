f0cad9cdb35d8c78344441f77258e26a2cbeda49
entaroadun <pawel.matykiewicz@gmail.com>
2011-11-23 22:22:45 -0500

    Development ver. moved to production

--- appengine_config.py
+++ appengine_config.py
@@ -3,4 +3,4 @@
 ## copy data from 
 ## one app to the other
 ## (http://code.google.com/appengine/docs/adminconsole/datastoreadmin.html)
-remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ('HTTP_X_APPENGINE_INBOUND_APPID', ['hnpickup'])
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ('HTTP_X_APPENGINE_INBOUND_APPID', ['hnpickupdev'])
