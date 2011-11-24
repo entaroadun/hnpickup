760853c92bbdb0ca8b362fdc768cf72dc4710f67
entaroadun <pawel.matykiewicz@gmail.com>
2011-11-23 21:01:31 -0500

    Added comments in CSS file, HTML file; removed paypal; datastore copy enabled

---
+++ appengine_config.py
@@ -0,0 +1,5 @@
+## this file and its content
+## is needed if you plan to 
+## copy data from 
+## one app to the other
+remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = ('HTTP_X_APPENGINE_INBOUND_APPID', ['hnpickup'])
